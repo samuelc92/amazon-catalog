9469178ec3a34c8f3b372e8eb238d9a271ef667f
Samuel Couto <samuel.couto92@gmail.com>
2023-05-30 22:47:49 +0200

    feat: adding Category and CategoryType

---
+++ Domain/Entities/Category.fs
@@ -0,0 +1,9 @@
+namespace Entities
+
+module Category =
+  
+  type T = { Id: int
+             Name: string
+             Description: string
+             CategoryType: CategoryType.T}
+             
---
+++ Domain/Entities/CategoryType.fs
@@ -0,0 +1,9 @@
+namespace Entities
+
+module CategoryType =
+  
+  type T = { Id: int
+             Name: string
+             Description: string
+             ShowOnMenu: bool
+             ShowOnHome: bool}
