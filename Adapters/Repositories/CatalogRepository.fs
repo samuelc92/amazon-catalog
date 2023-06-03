e8bd3319c4bc79d1e6d6ced042a5e4faa838d915
Samuel Couto <samuel.couto92@gmail.com>
2023-06-03 18:26:18 +0200

    refactor: deleting CatalogRepository class

--- Adapters/Repositories/CatalogRepository.fs
+++
@@ -1,9 +0,0 @@
-namespace Adapters.Repositories
-
-module CatalogRepository =
-  open Dapper.FSharp.PostgreSQL
-  open Npgsql
-
-  let conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=postgres;User Id=postgres;Password=postgres;")
-
-  let  = table<Product>
