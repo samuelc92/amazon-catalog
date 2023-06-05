a2843cab8e57e4bc7a84f704315483543d11710a
Samuel Couto <samuel.couto92@gmail.com>
2023-06-05 22:20:20 +0200

    feat: creating insert product query

--- src/Amazon.Catalog.Adapters/Data/Repositories/ProductRepository.fs
+++ src/Amazon.Catalog.Adapters/Data/Repositories/ProductRepository.fs
@@ -1,6 +1,21 @@
-namespace Amazon.Catalog.Adapters.Data.Repositories
+module Amazon.Catalog.Adapters.Data.Repositories
 
 module ProductRepository =
+  open Donald
   open Npgsql
 
+  open Amazon.Catalog.Core.Entities
+
   let conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=postgres;User Id=postgres;Password=postgres;")
+
+  let insertAsync(prod: Product.T) =
+    conn
+    |> Db.newCommand "INSERT INTO public.product VALUES (Id,Name,Description,Price,Active)"
+    |> Db.setParams [
+      "Id", SqlType.Guid prod.Id
+      "Name", SqlType.String prod.Name
+      "Description", SqlType.String prod.Description
+      "Price", SqlType.Decimal prod.Price
+      "Active", SqlType.Boolean prod.Active
+    ]
+    |> Db.Async.exec
