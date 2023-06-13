1e0709f69df15e145fb69632943487a422fcdb97
Samuel Couto <samuel.couto92@gmail.com>
2023-06-13 22:49:29 +0200

    chore: adjusting identation

--- src/Amazon.Catalog.WebApi/Controllers/BaseController.fs
+++ src/Amazon.Catalog.WebApi/Controllers/BaseController.fs
@@ -4,24 +4,24 @@ type ProblemDetails = { Title: string
                         Status: int 
                         Errors: string list }
 module BaseController =
-    open Falco
+  open Falco
     
-    open Amazon.Catalog.Core
-    open System.Text.Json
+  open Amazon.Catalog.Core
+  open System.Text.Json
  
-    let handleError (error: Error) : HttpHandler =
-      error
-      |> function
-        | DbError (message, _) -> Response.withStatusCode 500 >> Response.ofPlainText message
-        | NotFoundError _ -> Response.withStatusCode 404 >> Response.ofEmpty  
-        | DomainError message -> Response.withStatusCode 400 >> Response.ofPlainText message
+  let handleError (error: Error) : HttpHandler =
+    error
+    |> function
+      | DbError (message, _) -> Response.withStatusCode 500 >> Response.ofPlainText message
+      | NotFoundError _ -> Response.withStatusCode 404 >> Response.ofEmpty  
+      | DomainError message -> Response.withStatusCode 400 >> Response.ofPlainText message
 
-    let handleErrors (errors: Error list) : HttpHandler =
-      let jsonOption = JsonSerializerOptions()
-      jsonOption.PropertyNamingPolicy <- JsonNamingPolicy.CamelCase
-      let problemDetails = {
-        Title = "One or more validation errors occurred."
-        Status = 400
-        Errors = List.map (fun (err: Error) -> match err with | DomainError message -> message | _ -> "Internal server error.") errors 
-      }
-      Response.withStatusCode 400 >> Response.ofJsonOptions jsonOption problemDetails
+  let handleErrors (errors: Error list) : HttpHandler =
+    let jsonOption = JsonSerializerOptions()
+    jsonOption.PropertyNamingPolicy <- JsonNamingPolicy.CamelCase
+    let problemDetails = {
+      Title = "One or more validation errors occurred."
+      Status = 400
+      Errors = List.map (fun (err: Error) -> match err with | DomainError message -> message | _ -> "Internal server error.") errors 
+    }
+    Response.withStatusCode 400 >> Response.ofJsonOptions jsonOption problemDetails
