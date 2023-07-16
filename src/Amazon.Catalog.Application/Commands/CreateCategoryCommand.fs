8a1193fca3b998bfc5f34cd11ad9bd3e3532a0a8
Samuel Couto <samuel.couto92@gmail.com>
2023-07-16 19:56:58 +0200

    feat: validate whether category type exists on creating a new category

--- src/Amazon.Catalog.Application/Commands/CreateCategoryCommand.fs
+++ src/Amazon.Catalog.Application/Commands/CreateCategoryCommand.fs
@@ -5,6 +5,7 @@ module CreateCategoryCommand =
   open System
    
   open Amazon.Catalog.Adapters.Data.Repositories
+  open Amazon.Catalog.Core
   open Amazon.Catalog.Core.Entities
   open Amazon.Catalog.Core.Utils
    
@@ -20,7 +21,15 @@ module CreateCategoryCommand =
       ParentId=input.ParentId
       CategoryTypeId=input.CategoryTypeId }
 
+  let checkIfCategoryTypeIsValid (category: Category.T) =
+    CategoryTypeRepository.getById category.CategoryTypeId
+    |> function
+      | Ok (Some _)  -> Ok category
+      | Ok (None)    -> Error (DomainError "Invalid category type")
+      | Error err  -> Error err
+
   let handle (req: Input) =
     createEntity req
     |> Category.validate
+    >>= checkIfCategoryTypeIsValid
     >>= CategoryRepository.insert
