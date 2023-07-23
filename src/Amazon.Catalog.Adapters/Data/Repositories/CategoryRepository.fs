437b2216d67eb748495946649c71dbef1cb49ba7
Samuel Couto <samuel.couto92@gmail.com>
2023-07-23 22:53:50 +0200

    feat: create sql query to get category

--- src/Amazon.Catalog.Adapters/Data/Repositories/CategoryRepository.fs
+++ src/Amazon.Catalog.Adapters/Data/Repositories/CategoryRepository.fs
@@ -2,6 +2,7 @@ namespace Amazon.Catalog.Adapters.Data.Repositories
 
 module CategoryRepository =
   open Donald
+  open System.Data
 
   open Amazon.Catalog.Adapters.Data
   open Amazon.Catalog.Core
@@ -21,3 +22,22 @@ module CategoryRepository =
     |> function
       | Ok _      -> Ok () 
       | Error err -> err |> Helper.convertDbError
+
+  let ofDataReader (rd: IDataReader): Category.T =
+    { Id             = rd.ReadGuid       "id"
+      Name           = rd.ReadString     "name"
+      Description    = rd.ReadString     "description"
+      ParentId       = rd.ReadGuidOption "category_id"
+      CategoryTypeId = rd.ReadGuid       "category_type_id" }
+
+  let get limit offset : Result<Category.T list, Error> =
+    Database.conn
+    |> Db.newCommand "SELECT * FROM public.category LIMIT @Limit OFFSET @Offset  "
+    |> Db.setParams [
+      "@Limit", SqlType.Int limit
+      "@Offset", SqlType.Int offset 
+    ]
+    |> Db.query ofDataReader
+    |> function
+      | Ok catTypes -> Ok catTypes 
+      | Error err               -> err |> Helper.convertDbError
